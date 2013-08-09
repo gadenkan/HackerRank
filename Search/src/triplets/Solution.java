bcf69c7690ed66d8e6b29234e930be07d674a79a
punit <punitmg@gmail.com>
2013-08-09 16:58:49 -0400

    Triplets partial

--- Search/src/triplets/Solution.java
+++ Search/src/triplets/Solution.java
@@ -1,5 +1,7 @@
 package triplets;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
@@ -9,10 +11,33 @@ public class Solution {
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
 		int[] arr = new int[n];
-		for(int i=0;i<n;i++){
-			arr[i]=s.nextInt();
+		for (int i = 0; i < n; i++) {
+			arr[i] = s.nextInt();
 		}
-		
+		int len = arr.length;
+		int count = 0;
+		List<String> sign = new ArrayList<String>();
+		for (int i = 0; i < len; i++) {
+
+			for (int j = i; j < len; j++) {
+
+				if (arr[i] < arr[j]) {
+					for (int k = j; k < len; k++) {
+
+						if (arr[i] < arr[j] && arr[j] < arr[k]) {
+							//System.out.println(i + " " + j + " " + k);
+							String signTemp = "" + arr[i] + arr[j] + arr[k];
+							if (!sign.contains(signTemp)) {
+								count++;
+								sign.add(signTemp);
+							}
+						}
+					}
+				}
+			}
+		}
+
+		System.out.println(count);
 		s.close();
 	}
 
